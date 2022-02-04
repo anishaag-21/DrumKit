341eda0c9652f12e0acd41199318fd3b1cb560cb
anishaag-21 <96198785+anishaag-21@users.noreply.github.com>
2022-02-04 22:30:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+//RECOGNIZING BUTTON CLICKS
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonHtml = this.innerHTML;
+    makeSound(buttonHtml);
+    playAnimation(buttonHtml);
+  });
+}
+
+ 
+//RECOGNIZING KEY STROKES
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    playAnimation(event.key);
+});
+
+
+
+//FUNCTION TO IDENTIFY THE APPROPRIATE KEY AND MAKE SOUND ACCORDINGLY
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+        
+        case "a":
+          var audio = new Audio("sounds/tom-3.mp3");
+          audio.play(); 
+          break;
+  
+        case "s":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+  
+       case "d":
+          var audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+  
+        case "j":
+          var audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+  
+        case "k":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+  
+        case "l":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+          break;
+        case "m":
+          var audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+  
+        default: console.log(buttonHtml);
+          break;
+      }
+}
+
+function playAnimation(currentkey){
+    var activeKey = document.querySelector("." + currentkey);
+    activeKey.classList.add("pressed");
+    setTimeout(function(){activeKey.classList.remove("pressed")}, 100);
+}
